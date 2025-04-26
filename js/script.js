c61408091757073ecb1cd29f2283a8d6c68479ad
Arda <asoysuren91@gmail.com>
2025-04-26 15:09:49 +0200

    Change

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-console.log("hello world!");
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for reaching out! I'll get back to you soon.");
+});
